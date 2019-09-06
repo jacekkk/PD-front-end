a590cc0baa6e293217e75b0dc18041cfa61deaa7
JacasEdi <jacek.budzynski.92@gmail.com>
2019-09-06 22:30:51 +0100

    Refactor code & import bootstrap styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, useQuery, useMutation  } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import { Table, Button, Modal, Form } from 'react-bootstrap'
+import { Table, Button, Modal, Form, Jumbotron, Container } from 'react-bootstrap';
+import moment from 'moment'
 
-const USER_ID = '5d72a99fdd48d93e06c919d0'
+
+const USER_ID = '5d72be4d68e1cbfe6001bb49'
 
 const GET_USER = gql`
   query getUser($userId: String!)
@@ -32,11 +34,16 @@ const GET_USER = gql`
 const ADD_RUN = gql`
   mutation addRun(
     $userId: String!
-    $time: Int!
+    $time: Float!
     $distance: Float!
     $date: DateTime
   ) {
-    addRun (runInput: {userId: $userId, time: $time, distance: $distance, date: $date}) {
+    addRun (runInput: {
+      userId: $userId,
+      time: $time,
+      distance: $distance,
+      date: $date
+      }) {
       run {
         id
         time
@@ -71,33 +78,43 @@ const Runs = () => {
   });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error...</p>;
+  if (error) return <p>Error! Please make sure that user exists and that the value of USER_ID is correct.</p>;
 
   return (
-    <React.Fragment>
-      <Table striped bordered hover>
+    <Container fluid>
+      <Jumbotron>
+        <h1>Hi {data.user.firstName}!</h1>
+      </Jumbotron>
+      <Table>
         <thead>
           <tr>
             <th>Date</th>
-            <th>Distance</th>
-            <th>Time</th>
-            <th>Calories Burned</th>
+            <th>Distance (kilometres)</th>
+            <th>Time (hours)</th>
+            <th>Calories Burned (per minute)</th>
           </tr>
         </thead>
         <tbody>
-          {data.user.runs.map(run => 
-            <tr key={run.id}>
-              <td>{run.date}</td>
-              <td>{run.distance}</td>
-              <td>{run.time}</td>
-              <td>{run.caloriesBurned}</td>
-            </tr>  
-          )}
+          {data.user.runs.map(run => <Run key={run.id} runData={run}/>)}
         </tbody>
       </Table>
       <Button variant="primary" onClick={handleShow}>Add Run</Button>
       { show && <AddRunModal show={show} handleClose={handleClose}/> }
-    </React.Fragment>
+    </Container>
+  );
+}
+
+const Run = ({runData}) => {
+  const {date, distance, time, caloriesBurned} = runData;
+  const formattedDate = moment(date).format('YYYY-MM-DD hh:mm');
+
+  return (
+    <tr>
+      <td>{formattedDate}</td>
+      <td>{distance}</td>
+      <td>{time}</td>
+      <td>{caloriesBurned}</td>
+    </tr>
   );
 }
 
@@ -106,50 +123,61 @@ const AddRunModal = ({show, handleClose}) => {
   const [distance, setDistance] = useState(1);
   const [date, setDate] = useState(new Date().toISOString());
 
+  const handleSave = () => {
+    addRun()
+    .then(() => {
+      handleClose();
+      window.location.reload();
+    })
+    .catch(() =>
+      alert("Unable to add run, please check the input")
+    )
+  }
+
   const [addRun] = useMutation(ADD_RUN, {
     variables: {
         userId: USER_ID,
         time: time,
         distance: distance,
         date: date
-    },
+    }
   });
 
   return (
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show}>
         <Modal.Body>
-        <Form>
-          <label>
-            Time:
-            <input
-              type="text"
-              value={time}
-              onChange={e => setTime(e.target.value)}
-            />
-          </label>
-          <label>
-            Distance:
-            <input
-              type="text"
-              value={distance}
-              onChange={e => setDistance(e.target.value)}
-            />
-          </label>
-          <label>
-            Date:
-            <input
-              type="text"
-              value={date}
-              onChange={e => setDate(e.target.value)}
-            />
-          </label>
-        </Form>
+          <Form>
+            <label>
+              Time:
+              <input
+                type="number"
+                value={time}
+                onChange={e => setTime(e.target.value)}
+              />
+            </label>
+            <label>
+              Distance:
+              <input
+                type="number"
+                value={distance}
+                onChange={e => setDistance(e.target.value)}
+              />
+            </label>
+            <label>
+              Date:
+              <input
+                type="text"
+                value={date}
+                onChange={e => setDate(e.target.value)}
+              />
+            </label>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="primary" onClick={addRun}>
+          <Button variant="primary" onClick={handleSave}>
             Save
           </Button>
         </Modal.Footer>
